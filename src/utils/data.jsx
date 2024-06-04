b43bcd4e72a2b813333639a879915213d30e7bf0
Daniyal Anwar khan <77050423+khandanial419@users.noreply.github.com>
2024-06-04 19:10:03 +0500

    update

---
+++ src/utils/data.jsx
@@ -0,0 +1,20 @@
+export const faqsQuestion = [
+  {
+    question: "Q1: Is the website of Sleek assured removals secure?",
+    Answer1: "Is the website of Sleek assured removals secure?",
+    Answer2:
+      "The connection is encrypted via 256 bit SSL via Cloudflare for Sleek Assured Removals, and the payment modules are protected byStripe and PayPal, both of which are globally recognized and trusted",
+  },
+  {
+    question: "Q2: Is the website of Sleek assured removals secure?",
+    Answer1: "Is the website of Sleek assured removals secure?",
+    Answer2:
+      "The connection is encrypted via 256 bit SSL via Cloudflare for Sleek Assured Removals, and the payment modules are protected byStripe and PayPal, both of which are globally recognized and trusted",
+  },
+  {
+    question: "Q3: Is the website of Sleek assured removals secure?",
+    Answer1: "Is the website of Sleek assured removals secure?",
+    Answer2:
+      "The connection is encrypted via 256 bit SSL via Cloudflare for Sleek Assured Removals, and the payment modules are protected byStripe and PayPal, both of which are globally recognized and trusted",
+  },
+];
