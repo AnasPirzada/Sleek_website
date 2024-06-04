0699300691eb93374b4d229ec0ff427a00adb9e1
Daniyal Anwar khan <77050423+khandanial419@users.noreply.github.com>
2024-06-04 14:06:47 +0500

    update

--- src/routing/routing.jsx
+++ src/routing/routing.jsx
@@ -1,5 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+<<<<<<< Updated upstream
 import Index from "../../src/App/Components/Pages/Home/Index";
+=======
+import Index from "../App/Components/Pages/Home/index";
+>>>>>>> Stashed changes
 import Navbar from "../../src/App/Components/Narbar/index";
 import Blog from "../App/Components/Pages/Blog/Blog";
 export const router = createBrowserRouter([
